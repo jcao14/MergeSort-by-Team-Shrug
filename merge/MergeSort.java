b9b8509f17e1ca1f4b24751ed7cf7beb1710b259
James Cao <jcao14@stuy.edu>
2016-03-09 14:50:22 -0500

    Created MergeSort package

---
+++ merge/MergeSort.java
@@ -0,0 +1,139 @@
+/*======================================
+James Cao
+APCS2 pd9
+HW9 -- Mergers and Acquisitions
+2016-03-06
+
+  class MergeSort
+  Implements mergesort on array of ints.
+  Summary of Algorithm: 
+  ======================================*/
+
+public class MergeSort {
+
+   /******************************************************
+     * int[] merge(int[],int[]) 
+     * Merges two input arrays
+     * Precond:  Input arrays are sorted in ascending order
+     * Postcond: Input arrays unchanged, and 
+     * output array sorted in ascending order.
+     ******************************************************/
+    private static int[] merge( int[] a, int[] b ) 
+    {
+        int[] output = new int[a.length + b.length];
+        int counterA = 0;
+        int counterB = 0;
+        int index = 0;
+        
+        while ( ((counterA) < (a.length)) && ( (counterB) < (b.length) )){
+            if (a[counterA] < b[counterB]){
+                output[index] = a[counterA];
+                counterA ++;
+                index ++;
+            }
+            
+            else{
+                output[index] = b[counterB];
+                counterB ++;
+                index ++;
+            }
+        }
+        
+        if (counterA >= a.length){
+            for (int i = counterB; i < b.length; i++){
+                output [index]= b[i];
+                index ++;
+            }
+        }
+        
+        if (counterB >= b.length){
+            for (int i = counterA; i < a.length; i++){
+                output [index]= a[i];
+                index ++;
+            }
+        }
+        return output;
+    }//end merge()
+    
+
+
+    /******************************************************
+     * int[] sort(int[]) 
+     * Sorts input array using mergesort algorithm
+     * Returns sorted version of input array (ascending)
+     ******************************************************/
+    public static int[] sort( int[] arr ) 
+    {
+        if (arr.length == 1){
+            return arr;
+        }
+        
+        else{
+            int size = (int)(arr.length /2);
+            int[] a = new int[size];
+            int[] b = new int[arr.length - size];
+            
+            for (int i = 0; i < size; i++){
+                a[i] = arr[i];
+            }
+            
+            for (int i = size; i < arr.length; i++){
+                b[i - size] = arr[i];
+            }
+            return  merge(sort(a), sort(b));
+        }
+
+    }//end sort()
+
+
+
+    //-------------------HELPERS-------------------------
+    //tester function for exploring how arrays are passed
+    //usage: print array, mess(array), print array. Whaddayasee?
+    public static void mess( int[] a ) {
+	for( int i = 0 ; i<a.length; i++ )
+	    a[i] = 0;
+    }
+
+    //helper method for displaying an array
+    public static void printArray( int[] a ) {
+	System.out.print("[");
+	for( int i : a )
+	    System.out.print( i + ",");
+	System.out.println("]");
+    }
+    //---------------------------------------------------
+
+
+    //main method for testing
+    public static void main( String [] args ) {
+
+	
+	int[] arr0 = {0};
+	int[] arr1 = {1};
+	int[] arr2 = {1,2};
+	int[] arr3 = {3,4};
+	int[] arr4 = {1,2,3,4};
+	int[] arr5 = {4,3,2,1};
+	int[] arr6 = {9,42,17,63,0,512,23};
+	int[] arr7 = {9,42,17,63,0,9,512,23,9};
+	System.out.println("\nTesting mess-with-array method...");
+	printArray( arr3 );
+	mess(arr3);
+	printArray( arr3 );
+
+	System.out.println("\nMerging arr1 and arr0: ");
+	printArray( merge(arr1,arr0) );
+
+	System.out.println("\nMerging arr4 and arr6: ");
+	printArray( merge(arr4,arr6) );
+
+	System.out.println("\nSorting arr4-7...");
+	printArray( sort( arr4 ) );
+	printArray( sort( arr5 ) );
+	printArray( sort( arr6 ) );
+	printArray( sort( arr7 ) );
+
+    }//end main()
+
+}//end class MergeSort
