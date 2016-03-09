f0840ca432dd6fc30ccb5a15a148868556805709
James Cao <jcao14@stuy.edu>
2016-03-09 14:48:13 -0500

    added shuffle and populate

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -17,7 +17,26 @@
 
 public class MergeSortTester 
 {
-
+    //populates array
+    public static ArrayList populate( int size, int lo, int hi ) {
+	ArrayList<Integer> retAL = new ArrayList<Integer>();
+	while( size > 0 ) {
+	    //     offset + rand int on interval [lo,hi]
+	    retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
+	    size--;
+	}
+	return retAL;
+    }
+    //shuffles an array
+    public static void shuffle( ArrayList al ) {
+	int randomIndex;
+        for( int i = al.size()-1; i > -1; i-- ) {
+	    //pick an index at random
+            randomIndex = (int)( (i+1) * Math.random() );
+	    //swap the values at position i and randomIndex
+            al.set( i, al.set( randomIndex, al.get(i) ) );
+        }
+    }
     }//end sort()
     /******************************
      * execution time analysis 
